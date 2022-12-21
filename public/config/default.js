69360ccc34341b760993618af973de14b4afb9e2
fildal-1 <fildal-1@student.ltu.se>
2022-12-21 12:21:46 +0100

    Added javaHOST

--- public/config/default.js
+++ public/config/default.js
@@ -1,9 +1,11 @@
 // Webpage URL:
 
-const webHOST = "172.20.10.6";
+'use strict';
+
+const webHOST = "localhost";
 const webPORT = 3030;
 
-const javaHOST = "172.20.10.6";
+const javaHOST = "localhost";
 const javaPORT = 8080;
 
 exports.webHOST = webHOST;
