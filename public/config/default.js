d5fca65e696e720360675d1a20be5b1cb2c2e460
fildal-1 <fildal-1@student.ltu.se>
2022-12-22 16:38:09 +0100

    Added Auto setting Host IP. Serverside

--- public/config/default.js
+++ public/config/default.js
@@ -1,6 +1,35 @@
+/**
+ * Get local IP address, while ignoring vEthernet IP addresses (like from Docker, etc.)
+ */
+
+//#region Automatic detection of IPv4 address
 
 
-const webHOST = "192.168.1.67";
+var
+    // Local IP address that we're trying to calculate
+    ip_address
+    // Provides a few basic operating-system related utility functions (built-in)
+    ,os = require('os')
+    // Network interfaces
+    ,ifaces = os.networkInterfaces();
+
+
+// Iterate over interfaces ...
+for (var dev in ifaces) {
+
+    // ... and find the one that matches the criteria
+    var iface = ifaces[dev].filter(function(details) {
+        return details.family === 'IPv4' && details.internal === false;
+    });
+
+    if(iface.length > 0)
+        ip_address = iface[0].address;
+}
+
+//#endregion Automatic detection of IPv4 address
+
+
+const webHOST = ip_address;
 const webPORT = 3030;
 
 const javaHOST = webHOST;
